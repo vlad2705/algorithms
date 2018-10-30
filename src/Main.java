578ec82c4f8af898ece939ec491f66c5fad09874
Vladislav Rudenko <vladislav.rudenko@moneta.cz>
2018-10-30 21:31:51 +0100

    create selection sort

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+import sort.SelectionSort;
+
+public class Main {
+  public static void main(String[] args) {
+    int[] lst = {4, 9, 7, 1, 3, 6, 5};
+    SelectionSort.selectionSort(lst);
+  }
+}
---
+++ src/sort/SelectionSort.java
@@ -0,0 +1,31 @@
+package sort;
+
+import java.util.Arrays;
+
+public class SelectionSort {
+  public static void selectionSort(int[] lst) {
+    for (int i = 0; i < lst.length - 1; ++i) {
+      int indexOfMinValue = getIndexOfMinValue(i, lst);
+      swap(i, indexOfMinValue, lst);
+    }
+    System.out.println(Arrays.toString(lst));
+  }
+  
+  private static int getIndexOfMinValue(int startIndex, int[] lst) {
+    int smallest = lst[startIndex];
+    int index = startIndex;
+    for (int j = startIndex + 1; j < lst.length; ++j) {
+      if (lst[j] < smallest) {
+        smallest = lst[j];
+        index = j;
+      }
+    }
+    return index;
+  }
+  
+  private static void swap(int firstIndex, int secondIndex, int[] lst) {
+    int tmp = lst[firstIndex];
+    lst[firstIndex] = lst[secondIndex];
+    lst[secondIndex] = tmp;
+  }
+}
