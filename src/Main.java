ac38b0ab8e93ad7f44027d17ba78de92e8ab8741
Vladislav Rudenko <vladislav.rudenko@moneta.cz>
2018-10-31 07:53:10 +0100

    create merge sort

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,10 @@ import sort.Sort;
 
 public class Main {
   public static void main(String[] args) {
-    int[] lst = {4, 9, 7, 1, 3, 6, 5};
-    Sort.selectionSort(lst);
-    Search.linearSearch(lst, 5);
-    Sort.bubbleSort(lst);
+    int[] lst1 = {4, 9, 7, 1, 3, 6, 5};
+    Sort.selectionSort(lst1);
+    Search.linearSearch(lst1, 5);
+    Sort.bubbleSort(lst1);
+    Sort.mergeSort(lst1);
   }
 }
--- src/sort/Sort.java
+++ src/sort/Sort.java
@@ -25,6 +25,30 @@ public class Sort {
     System.out.println(Arrays.toString(lst));
   }
   
+  public static int[] mergeSort(int[] lst) {
+    if (lst.length < 2) return lst;
+    int[] left, right;
+    if (lst.length % 2 == 0) {
+      left = new int[lst.length / 2];
+      right = new int[lst.length / 2];
+    } else {
+      left = new int[lst.length / 2];
+      right = new int[lst.length / 2 + 1];
+    }
+    for (int i = 0; i < lst.length; ++i) {
+      if (i < lst.length / 2) {
+        left[i] = lst[i];
+      } else {
+        right[i - lst.length / 2] = lst[i];
+      }
+    }
+    left = mergeSort(left);
+    right = mergeSort(right);
+    lst = merge(left, right);
+    System.out.println(Arrays.toString(lst));
+    return lst;
+  }
+  
   private static int getIndexOfMinValue(int startIndex, int[] lst) {
     int smallest = lst[startIndex];
     int index = startIndex;
@@ -41,5 +65,24 @@ public class Sort {
     int tmp = lst[firstIndex];
     lst[firstIndex] = lst[secondIndex];
     lst[secondIndex] = tmp;
+  }
+  
+  private static int[] merge(int[] left, int[] right) {
+    int[] result = new int[left.length + right.length];
+    int i = 0, j = 0, index = 0;
+    while (i < left.length && j < right.length) {
+      if (left[i] < right[j]) {
+        result[index++] = left[i++];
+      } else{
+          result[index++] = right[j++];
+      }
+    }
+    while (i < left.length) {
+      result[index++] = left[i++];
+    }
+    while(j < right.length) {
+      result[index++] = right[j++];
+    }
+    return result;
   }
 }
